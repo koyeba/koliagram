f8339675bf6b72bb3b96e3f53042b2e37b66b295
Albert <albert.tumba@gmail.com>
2025-06-09 15:40:09 +0200

    style: add mobile menu transition slide-down

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -7,7 +7,7 @@
     aria-label="Navigation principale"
   >
     <div
-      class="max-w-[100rem] mx-auto flex justify-between items-center px-6 sm:px-8 md:px-12 lg:px-16 xl:px-20 2xl:px-24 pt-4 pb-2 transition-padding duration-300"
+      class="max-w-[100rem] mx-auto flex justify-between items-center px-6 sm:px-8 md:px-12 lg:px-16 xl:px-20 2xl:px-24 py-4 transition-padding duration-300"
     >
       <!-- Logo -->
       <NuxtLink to="/" aria-label="Accueil" class="flex items-center">
@@ -24,7 +24,7 @@
       </NuxtLink>
 
       <!-- Desktop menu -->
-      <div class="flex md:flex items-center gap-1 md:gap-4">
+      <div class="flex md:flex items-center gap-3 md:gap-4">
         <NuxtLink
           to="/login"
           aria-label="Connexion"
@@ -43,7 +43,7 @@
         <!-- Mobile menu button -->
         <button
           @click="isMobileMenuOpen = !isMobileMenuOpen"
-          class="md:hidden flex items-center justify-center p-2 z-70"
+          class="md:hidden flex items-center justify-center z-70"
           aria-label="Menu mobile"
         >
           <svg
@@ -81,35 +81,37 @@
     </div>
 
     <!-- Mobile menu -->
-    <div
-      v-if="isMobileMenuOpen"
-      class="md:hidden fixed inset-0 h-screen z-60 px-12 overflow-hidden flex flex-col items-start gap-4 bg-white backdrop-blur-lg pt-14 text-xl font-display font-medium tracking-tight text-[#333336]"
-    >
-      <NuxtLink
-        to="/login"
-        @click="isMobileMenuOpen = false"
-        aria-label="Connexion"
-        class=""
+    <transition name="slide-down">
+      <nav
+        v-if="isMobileMenuOpen"
+        class="md:hidden fixed inset-0 h-screen z-60 px-12 overflow-hidden flex flex-col items-start gap-4 bg-white backdrop-blur-lg pt-14 text-xl font-display font-medium tracking-tight text-[#333336]"
       >
-        Se connecter
-      </NuxtLink>
-      <NuxtLink
-        to="/"
-        @click="isMobileMenuOpen = false"
-        aria-label="Réservez votre démo"
-        class=""
-      >
-        Réservez votre démo
-      </NuxtLink>
-      <NuxtLink
-        to="/register"
-        @click="isMobileMenuOpen = false"
-        aria-label="S'inscrire"
-        class=""
-      >
-        S'inscrire
-      </NuxtLink>
-    </div>
+        <NuxtLink
+          to="/login"
+          @click="isMobileMenuOpen = false"
+          aria-label="Connexion"
+          class=""
+        >
+          Se connecter
+        </NuxtLink>
+        <NuxtLink
+          to="/"
+          @click="isMobileMenuOpen = false"
+          aria-label="Réservez votre démo"
+          class=""
+        >
+          Réservez votre démo
+        </NuxtLink>
+        <NuxtLink
+          to="/register"
+          @click="isMobileMenuOpen = false"
+          aria-label="S'inscrire"
+          class=""
+        >
+          S'inscrire
+        </NuxtLink>
+      </nav>
+    </transition>
   </nav>
 </template>
 
@@ -140,3 +142,22 @@ watch(isMobileMenuOpen, (val) => {
   }
 });
 </script>
+
+<style scoped>
+.slide-down-enter-active,
+.slide-down-leave-active {
+  transition: all 0.5s ease-in-out;
+}
+.slide-down-enter-from {
+  transform: translateY(-100%);
+}
+.slide-down-enter-to {
+  transform: translateY(0);
+}
+.slide-down-leave-from {
+  transform: translateY(0);
+}
+.slide-down-leave-to {
+  transform: translateY(-100%);
+}
+</style>
